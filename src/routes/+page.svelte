6e21735e6803d342a3099e864e43b676fecd00fc
Sean Thomas <aurorasean@gmail.com>
2025-01-09 18:25:04 +0000

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,14 +17,27 @@
         carousel.goToNext();
     };
 
-    const currentWeek = Math.ceil(
-        (new Date().getTime() -
-            new Date(new Date().getFullYear(), 0, 1).getTime()) /
-            (7 * 24 * 60 * 60 * 1000),
-    );
     let currentSelectedYear = new Date().getFullYear().toString();
 
+    const getWeekNumber = (d: Date) => {
+        // Copy date so don't modify original
+        d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+        // Set to nearest Thursday: current date + 4 - current day number
+        // Make Sunday's day number 7
+        d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+        // Get first day of year
+        var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+        // Calculate full weeks to nearest Thursday
+        var weekNo = Math.ceil(
+            (d.getTime() - yearStart.getTime()) / 86400000 / 7,
+        );
+        // Return array of year and week number
+        return weekNo;
+    };
+
+    const currentWeek = getWeekNumber(new Date()) - 1;
     boolStore.subscribe((value) => {
+        console.log({ value, currentWeek });
         if (value && carousel) {
             carousel.goTo(currentWeek);
         }
